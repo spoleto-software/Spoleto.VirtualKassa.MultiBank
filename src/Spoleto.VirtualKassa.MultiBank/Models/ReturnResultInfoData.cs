e79ecc36d969116e8a00901492a2f6622b1efb6f
o.nadymov <o.nadymov@spoleto.ru>
2023-10-30 19:26:39 +0300

    Fixes in models

--- src/Spoleto.VirtualKassa.MultiBank/Models/ReturnResultInfoData.cs
+++ src/Spoleto.VirtualKassa.MultiBank/Models/ReturnResultInfoData.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using Spoleto.VirtualKassa.MultiBank.Converters;
 
 namespace Spoleto.VirtualKassa.MultiBank.Models
 {
@@ -17,13 +18,14 @@ namespace Spoleto.VirtualKassa.MultiBank.Models
         public int ReceiptSum { get; set; }
 
         [JsonPropertyName("receipt_gnk_receivedcash")]
-        public int ReceiptGnkReceivedcash { get; set; }
+        public int ReceiptGnkReceivedCash { get; set; }
 
         [JsonPropertyName("receipt_gnk_receivedcard")]
-        public int ReceiptGnkReceivedcard { get; set; }
+        public int ReceiptGnkReceivedCard { get; set; }
 
         [JsonPropertyName("receipt_gnk_time")]
-        public string ReceiptGnkTime { get; set; }
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime ReceiptGnkTime { get; set; }
 
         [JsonPropertyName("items")]
         public List<SlipResultItemInfo> Items { get; set; }
@@ -38,16 +40,32 @@ namespace Spoleto.VirtualKassa.MultiBank.Models
         public bool UseWarehouse { get; set; }
 
         [JsonPropertyName("receipt_gnk_receiptseq")]
-        public int ReceiptGnkReceiptseq { get; set; }
+        public int ReceiptGnkReceiptSeq { get; set; }
 
         [JsonPropertyName("module_terminalid")]
-        public string ModuleTerminalid { get; set; }
+        public string ModuleTerminalId { get; set; }
 
         [JsonPropertyName("module_gnk_id")]
         public string ModuleGnkId { get; set; }
 
         [JsonPropertyName("app")]
         public string App { get; set; }
+
+        [JsonPropertyName("receipt_gnk_terminalid")]
+        public string ReceiptGnkTerminalId { get; set; }
+
+        [JsonPropertyName("receipt_gnk_qrcodeurl")]
+        public string ReceiptGnkQrcodeUrl { get; set; }
+
+        [JsonPropertyName("receipt_gnk_fiscalsign")]
+        public string ReceiptGnkFiscalSign { get; set; }
+
+        [JsonPropertyName("receipt_gnk_appletversion")]
+        public string ReceiptGnkAppletVersion { get; set; }
+
+        [JsonPropertyName("receipt_gnk_datetime")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime? ReceiptGnkDatetime { get; set; }
 
         public override string ToString() => $"{nameof(ModuleGnkId)} = {ModuleGnkId}, {nameof(ReceiptCashierName)} = {ReceiptCashierName}, {nameof(ReceiptSum)} = {ReceiptSum}";
     }
